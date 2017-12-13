41f7c4b308465c88cba87d438f599a3a61432355
Monah <30553078+monahsilva@users.noreply.github.com>
2017-12-13 10:05:15 -0200

    Calculadora Simples
    Soma e subtração.

---
+++ calculadora.py
@@ -0,0 +1,8 @@
+print ("Hello World!!")
+
+#Calculadora
+
+val1 = eval(input("Entre com o primeiro valor"))
+val2 = eval(input("Entre com o segundo valor"))
+
+op = eval(input("Escolha a opção de operação:\n 1 - Soma \n 2 - Subtração \n 3 - Multiplicação \n 4 - Divisão \n  "))
